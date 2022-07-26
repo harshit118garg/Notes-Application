476333505270844ffaad5d258ba5f0b8386cb212
Harshit Garg <harshit118garg@gmail.com>
2022-07-26 10:50:45 +0530

    front-end setup ✌🙌👍

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="container bg-primary bg-opacity-25">
+        <div className="row">
+          <div className="col">
+            <section className='text-center'>
+              <h2 className="display-3">App JSX</h2>
+            </section>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
