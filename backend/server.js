333a0b7c59d0acb3493383c83368f71e01c18c81
Harshit Garg <harshit118garg@gmail.com>
2022-07-30 13:13:55 +0530

    mongoDB Atlas conn established ✔✌✅👍

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require('cors');
 const router = require('./router/routes');
+const connectDB = require("./dbconn/db");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 dotenv.config();
+
+connectDB();
 
 app.use(cors(), router);
 
